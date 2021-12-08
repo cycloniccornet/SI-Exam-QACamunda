423bd3fe216cbbdf199b2309d00eafc9790170b9
Tureczek <n.m.tureczek@gmail.com>
2021-12-08 13:47:17 +0100

    Process.bpmn created

---
+++ QandAService/QandAService/src/main/java/si/camunda/workflow/LoggerDelegate.java
@@ -0,0 +1,20 @@
+package si.camunda.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import javax.inject.Named;
+import java.util.logging.Logger;
+
+@Named("Logger")
+public class LoggerDelegate implements JavaDelegate {
+    private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        LOGGER.info("\n\n ... Logger Delegate invoked by " +
+                "processDefinitionId=" + execution.getProcessDefinitionId()
+                + ", activityId=" + execution.getCurrentActivityId());
+
+    }
+}
