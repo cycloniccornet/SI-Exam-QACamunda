9db7ae3502de37c0ad2ce36c7130e79409f57cdf
Tureczek <n.m.tureczek@gmail.com>
2021-12-10 12:03:37 +0100

    Pushed with QuestionDTO

---
+++ QandAService/QandAService/src/main/java/Model/QuestionDTO.java
@@ -0,0 +1,46 @@
+package Model;
+/*
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class QuestionDTO {
+    @Id
+    private int id;
+    private String topic;
+    private String question;
+
+    public QuestionDTO() {
+    }
+
+    public QuestionDTO(int id, String topic, String question) {
+        this.id = id;
+        this.topic = topic;
+        this.question = question;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
+*/
